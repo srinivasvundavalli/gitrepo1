0632bb94032baba8a458f16556bd9f5130a21575
srinivas vundavalli <srinivas.vundavalli@gmail.com>
2013-09-22 12:53:08 +0530

    Second commit

---
+++ GitProject/src/com/cnu/git/Welcome2Git.java
@@ -0,0 +1,14 @@
+package com.cnu.git;
+
+public class Welcome2Git {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("welcome to Git");
+
+	}
+
+}
